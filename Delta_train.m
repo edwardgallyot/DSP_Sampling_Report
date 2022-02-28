fcfaa8f8870e6ffd6009ee6b570f17aec38fa381
edgallyot <edgallyot@gmail.com>
2022-02-28 12:41:20 +0000

    Changes to make sampling rate better

--- Delta_train.m
+++ Delta_train.m
@@ -1,15 +1,18 @@
-Fs = 5;
+sampleRate = 20;
+timePerSample = 1 / sampleRate;
+lengthOfX = 1;
 
-n = linspace(0, Fs - 1, Fs);
-t = [0:0.01:Fs];
+n = [0:timePerSample:lengthOfX];
 
-x_t = zeros(Fs, 1);
+t = [0:0.001:lengthOfX];
+
+x_t = zeros(length(n), 1);
 x_t = x_t + 1;
 
 stem(n, x_t);
 
 hold on;
-freq = 5;
+freq = sampleRate;
 
 wave_n = cos(2 * pi * freq * n);
 wave_t = cos(2 * pi * freq * t);
@@ -18,5 +21,3 @@ plot(t, wave_t);
 stem(n, wave_n);
 
 hold off;
-
-X_f = fft()
